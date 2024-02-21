3710f2ca225d169530ad4c4710dc7e7a44b76612
Dominic Feightner <125988930+dfeightner@users.noreply.github.com>
2024-02-20 19:20:20 -0500

    Edit Functionality
    Edit functionality for Books

--- BookController.cs
+++ BookController.cs
@@ -6,16 +6,24 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace BooksApplication.Controllers
+namespace BooksApplication.Areas.Admin.Controllers
 {
+
+    [Area("Admin")]
     public class BookController : Controller
     {
 
         private BooksDBContext _dbContext;
+        private IWebHostEnvironment _environment;
 
-        public BookController(BooksDBContext context)
+
+
+
+        public BookController(BooksDBContext context, IWebHostEnvironment environment)
         {
             _dbContext = context;//passes the dbContext object to the instance variable
+            _environment = environment;
+
         }
 
         public IActionResult Index()
@@ -25,15 +33,15 @@ namespace BooksApplication.Controllers
             return View(ListOfBooks);
         }
 
-        [HttpGet] 
-        public IActionResult Create() 
+        [HttpGet]
+        public IActionResult Create()
         {
             IEnumerable<SelectListItem> listOfCategories = _dbContext.Categories.ToList().Select(o => new SelectListItem
-                 {
-                     Text = o.Name,
-                     Value = o.CategoryId.ToString()
+            {
+                Text = o.Name,
+                Value = o.CategoryId.ToString()
 
-                  }); //projection: allows us to project a categroy object to a selectlistitem object, where the name of the category is used as the text and the categoryId is used as the value of the SelectListItem
+            }); //projection: allows us to project a category object to a selectlistitem object, where the name of the category is used as the text and the categoryId is used as the value of the SelectListItem
 
             //1) viewbag
             //ViewBag.listOfCategories = listOfCategories;
@@ -53,10 +61,28 @@ namespace BooksApplication.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(BookWithCategoriesVM bookWithCategoriesVM)
+        public IActionResult Create(BookWithCategoriesVM bookWithCategoriesVM, IFormFile imgFile)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
+                string wwwrootPath = _environment.WebRootPath;
+                if(imgFile != null)
+                {
+                    using (var filestream = new FileStream(Path.Combine(wwwrootPath,@"Images\bookImages\" + imgFile.FileName), FileMode.Create))        {
+
+                        imgFile.CopyTo(filestream);//saves the file in the specified folder
+                    
+                    
+                    
+                   }
+
+                    bookWithCategoriesVM.Book.ImgUrl = @"\Images\bookImages\" + imgFile.FileName;
+
+
+
+                }
+
+
                 _dbContext.Books.Add(bookWithCategoriesVM.Book);
                 _dbContext.SaveChanges();
 
@@ -66,5 +92,52 @@ namespace BooksApplication.Controllers
 
             return View(bookWithCategoriesVM);
         }
+
+        public BooksDBContext Get_dbContext()
+        {
+            return _dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Book books = _dbContext.Books.Find(id);
+
+
+            IEnumerable<SelectListItem> listOfCategories = _dbContext.Categories.ToList().Select(o => new SelectListItem
+            {
+                Text = o.Name,
+                Value = o.CategoryId.ToString()
+
+            });
+
+            BookWithCategoriesVM bookWithCategoriesVM = new BookWithCategoriesVM();
+
+            bookWithCategoriesVM.Book = books;
+
+            bookWithCategoriesVM.ListOfCategories = listOfCategories;
+
+            return View(bookWithCategoriesVM);
+
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("BookId, BookTitle, Author, Description, Price, ImgUrl, CategoryId")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Books.Update(book);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index", "Book");
+            }
+
+            return View(book);
+
+        }
+
+
     }
 }
